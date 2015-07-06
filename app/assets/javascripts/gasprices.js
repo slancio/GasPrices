33fe0e26375ca748cfafefb3fa266225084d6541
Eric Savadian <eric.j.savadian@gmail.com>
2015-07-06 04:54:42 -0400

    Automatically search the first state in the list on load.

--- app/assets/javascripts/gasprices.js
+++ app/assets/javascripts/gasprices.js
@@ -18,19 +18,19 @@
 
     req.open("GET", url, true);
     req.send();
-  };
+  }
 
   function displayPriceData(data) {
     insertAtId('gas-state', data.states.name);
     insertAtId('gas-price', "$" + data.states.current_price + "/gal");
     displayLastUpdated(data);
-  };
+  }
 
   function displayLastUpdated(priceData) {
     var dateString = new Date(Date.parse(priceData.last_updated));
     dateString = dateString.toUTCString().split(' ').splice(0, 4).join(' ');
     insertAtId('gas-date', dateString);
-  };
+  }
 
   function buildStateList(JSONdata) {
     JSONdata.states.forEach(function(el, idx) {
@@ -40,19 +40,24 @@
     });
 
     var stateList = document.getElementById('gas-state-select');
+    getStateData(stateList);
     stateList.onchange = function () {
-      var selectedState = stateList.options[stateList.selectedIndex].value; 
-      fetchJSON(apiURL + "/" + selectedState, displayPriceData);
+      getStateData(stateList);
     };
-  };
+  }
+
+  function getStateData(stateList) {
+    var selectedState = stateList.options[stateList.selectedIndex].value;
+    fetchJSON(apiURL + "/" + selectedState, displayPriceData);
+  }
 
   function insertAtId(id, content) {
     document.getElementById(id).innerHTML = content;
-  };
+  }
 
   function appendAtId(id, content) {
     document.getElementById(id).innerHTML += content;
-  };
+  }
 
   fetchJSON(apiURL, buildStateList);
 })();
